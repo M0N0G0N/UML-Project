a01bcb369d4e9fb58c940d8915a5b45c9935fb57
Justinewg <73750498+Justinewg@users.noreply.github.com>
2022-03-10 14:21:23 -0500

    waiting plane menu

---
+++ PLANE.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class PLANE {
+    String name;
+    int Total_Passengers;
+    int Dead_Passengers;
+    int Fuel_Left;
+    int Refuel_Time;
+
+    public PLANE() {
+        //TODO: make a name randomizer
+        //TODO: make a Total Passenger randomizer
+        Dead_Passengers = 0;
+        //TODO: make a Fuel_Left randomizer
+        //TODO: make a Refuel_Time randomizer
+    }
+
+    public void Add_Fuel(int Fuel_Added){
+
+    }
+
+    public void Kill_Passengers(int Killed_Passengers) {
+
+    }
+
+    public void Kill_Plane(boolean Kill){
+
+    }
+}
